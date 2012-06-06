eb3831e478bb3109319e7ec4147178f7be276148
smyx <smyx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-06 02:25:22 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-connect/tags/2.3.5@553647 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login.php
+++ login.php
@@ -6,7 +6,7 @@ if (isset($_GET['go'])) {
 	$open_url = "http://open.denglu.cc/transfer/" . $name . "?appid=" . $wptm_basic['appid'];
 	$_SESSION['wp_url_login'] = $name;
 	header('Location:' . $open_url);
-} elseif ($_SERVER['HTTP_REFERER'] && isset($_GET['user_id'])) {
+} elseif (is_user_logged_in() && $_SERVER['HTTP_REFERER'] && isset($_GET['user_id'])) {
 	$user_id = $_GET['user_id'];
 	if ($_SESSION['user_id'] != $user_id) {
 		header('Location:' . $_SERVER['HTTP_REFERER']);
