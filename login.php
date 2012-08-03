dab33dcd25b9919405063f4c1a98e551c9d4c45d
smyx <smyx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-03 10:11:13 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-connect/trunk@581272 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- login.php
+++ login.php
@@ -1,20 +1,24 @@
 <?php
 include "../../../wp-config.php";
+session_start();
 if (isset($_GET['go'])) {
 	$name = strtolower($_GET['go']);
-	$wptm_basic = get_option('wptm_basic');
-	$open_url = "http://open.denglu.cc/transfer/" . $name . "?appid=" . $wptm_basic['appid'];
-	$_SESSION['wp_url_login'] = $name;
-	header('Location:' . $open_url);
-} elseif (is_user_logged_in() && $_SERVER['HTTP_REFERER'] && isset($_GET['user_id'])) {
+	if (in_array($name, array('qzone', 'sina', 'tencent', 'renren', 'taobao', 'douban', 'baidu', 'kaixin001', 'sohu', 'netease', 'tianya', 'windowslive', 'alipayquick', 'google', 'yahoo', 'netease163', 'twitter', 'facebook', 'tianyi', 'guard360'))) {
+		$_SESSION['wp_url_login'] = $name;
+		$wptm_basic = get_option('wptm_basic');
+		if (!empty($_SERVER['HTTP_REFERER'])) $redirect_uri = "&redirect_uri=" . urlencode($_SERVER['HTTP_REFERER']);
+		$open_url = "http://open.denglu.cc/transfer/" . $name . "?appid=" . $wptm_basic['appid'] . $redirect_uri;
+		header('Location:' . $open_url);
+	} 
+} elseif (is_user_logged_in() && !empty($_SERVER['HTTP_REFERER']) && isset($_GET['user_id'])) {
 	$user_id = $_GET['user_id'];
 	if (!$user_ID) $user_ID = get_current_user_id();
 	if ($user_id && ($user_ID == $user_id || current_user_can('manage_options'))) {
 		if (isset($_GET['bind'])) { // 登录绑定
 			$bind = strtolower($_GET['bind']);
-			$wptm_basic = get_option('wptm_basic');
-			$open_url = "http://open.denglu.cc/transfer/" . $bind . "?appid=" . $wptm_basic['appid'] . '&uid=' . $user_id;
 			$_SESSION['wp_url_login'] = '';
+			$wptm_basic = get_option('wptm_basic');
+			$open_url = "http://open.denglu.cc/transfer/" . $bind . "?appid=" . $wptm_basic['appid'] . '&uid=' . $user_id . "&redirect_uri=" . $_SERVER['HTTP_REFERER'];
 			header('Location:' . $open_url);
 		} elseif (isset($_GET['del'])) { // 解除绑定
 			$delete = strtolower($_GET['del']);
