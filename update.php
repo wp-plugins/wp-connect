9d23a062a9cc7cf2dffc4c85095fb66cd887ff0a
smyx <smyx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-05 09:47:29 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-connect/trunk@594839 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- update.php
+++ update.php
@@ -76,7 +76,7 @@ function wp_option_account() {
 		'sina' => get_option('wptm_sina'),
 		'sohu' => get_option('wptm_sohu'),
 		'netease' => get_option('wptm_netease'),
-		'twitter' => get_option('wptm_twitter_oauth'),
+		'twitter' => get_option('wptm_twitter'),
 		'shuoshuo' => get_option('wptm_shuoshuo'),
 		'renren' => get_option('wptm_renren'),
 		'kaixin001' => get_option('wptm_kaixin001'),
@@ -199,7 +199,7 @@ function wp_connect_update() {
 		'oauth_token_secret' => trim($_POST['password'])
 		);
 	if (isset($_POST['update_twitter'])) {
-		update_option("wptm_twitter_oauth", $token);
+		update_option("wptm_twitter", $token);
 		echo $updated;
 	} 
 	if (isset($_POST['update_qq'])) {
@@ -256,7 +256,7 @@ function wp_connect_update() {
 	} 
 	// delete
 	if (isset($_POST['delete_twitter'])) {
-		update_option("wptm_twitter_oauth", '');
+		update_option("wptm_twitter", '');
 	} 
 	if (isset($_POST['delete_qq'])) {
 		update_option("wptm_qq", '');
@@ -315,7 +315,7 @@ function wp_usermeta_account($uid) {
 		'sina' => $user -> wptm_sina,
 		'sohu' => $user -> wptm_sohu,
 		'netease' => $user -> wptm_netease,
-		'twitter' => $user -> wptm_twitter_oauth,
+		'twitter' => $user -> wptm_twitter,
 		'shuoshuo' => $user -> wptm_shuoshuo,
 		'renren' => $user -> wptm_renren,
 		'kaixin001' => $user -> wptm_kaixin001,
@@ -337,7 +337,7 @@ function wp_user_profile_update($user_id) {
 		'oauth_token_secret' => trim($_POST['password'])
 		);
 	if (isset($_POST['update_twitter'])) {
-		update_usermeta($user_id, "wptm_twitter_oauth", $token);
+		update_usermeta($user_id, "wptm_twitter", $token);
 	} 
 	if (isset($_POST['update_qq'])) {
 		update_usermeta($user_id, "wptm_qq", $token);
@@ -380,7 +380,7 @@ function wp_user_profile_update($user_id) {
 	} 
 	// delete
 	if (isset($_POST['delete_twitter'])) {
-		update_usermeta($user_id, 'wptm_twitter_oauth', '');
+		update_usermeta($user_id, 'wptm_twitter', '');
 	} 
 	if (isset($_POST['delete_qq'])) {
 		update_usermeta($user_id, 'wptm_qq', '');
